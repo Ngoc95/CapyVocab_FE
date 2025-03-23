045ede72cb3e4d8b47016b68acb3873fbcc02202
Dương Khánh Ngọc <duongkhanhngoc2005@gmail.com>
2025-03-23 13:28:21 +0700

    SplashScreen
    add: splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Splash Api
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
     // Accompanist (Hỗ trợ UI nâng cao)
